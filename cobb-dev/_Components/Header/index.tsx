f8136c1c5a70f5d175dee41565400cfc0732749c
Chiho <minepanorama@naver.com>
2022-08-19 18:08:44 +0900

    ADD: Material-ui 라이브러리 사용 시작

--- cobb-dev/_Components/Header/index.tsx
+++ cobb-dev/_Components/Header/index.tsx
@@ -1,12 +1,31 @@
-import ThemeToggle from '_Components/ThemeToggle'
+import { useState } from 'react'
+
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 import styles from './header.module.scss'
 
 function Header() {
+  const [isMenu, setIsMenu] = useState(false)
+
+  const handleOpenMenu = () => setIsMenu((prev) => !prev)
+
   return (
-    <header className={styles.globalHeader}>
-      This Is Global Header
-      <ThemeToggle />
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            Cobb.dev의 블로그
+          </Typography>
+          <IconButton size='large' edge='start' color='inherit' aria-label='menu'>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
