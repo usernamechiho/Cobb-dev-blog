589ea0ba9c003df6c4415883e4f1a415bc18802a
Chiho <minepanorama@naver.com>
2022-08-30 23:26:59 +0900

    ADD: index.html scss 모듈 추가

--- cobb-dev/pages/index.tsx
+++ cobb-dev/pages/index.tsx
@@ -1,5 +1,7 @@
+import styles from './home.module.scss'
+
 function Home() {
-  return <div style={{ height: '100vh' }}>HELLO My name is Chiho</div>
+  return <div className={styles.homeContainer}>HELLO My name is Chiho</div>
 }
 
 export default Home
