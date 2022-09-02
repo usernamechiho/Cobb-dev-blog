b9bddb4f76223c365abef2971fd5c4aa42174ecf
Chiho <minepanorama@naver.com>
2022-09-02 23:55:41 +0900

    MOD: type Test componenet 삭제

--- cobb-dev/pages/index.tsx
+++ cobb-dev/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './home.module.scss'
 
 function Home() {
-  return <div className={styles.homeContainer}>HELLO My name is Chiho</div>
+  return <div className={styles.homeContainer}>HELLO</div>
 }
 
 export default Home
