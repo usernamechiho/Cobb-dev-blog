5a6662872f7ca830c4ced1041a30ca4df3fb4120
Chiho <minepanorama@naver.com>
2022-09-28 19:19:46 +0900

    ADD: 이메일 전송 구현 완료

--- cobb-dev/pages/send-email/Components/EmailForm/index.tsx
+++ cobb-dev/pages/send-email/Components/EmailForm/index.tsx
@@ -1,34 +1,107 @@
 import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import SendRoundedIcon from '@mui/icons-material/SendRounded';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Button from '@mui/material/Button';
+import emailjs from '@emailjs/browser';
+import { useState, MouseEvent, ChangeEvent, useEffect } from 'react';
 import styles from './emailForm.module.scss';
 
 const EmailForm = () => {
+  const [title, setTitle] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value);
+  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value);
+  const handleChangeMessage = (e: ChangeEvent<HTMLTextAreaElement>) => setMessage(e.currentTarget.value);
+
+  const notifyError = () =>
+    toast.error('입력을 확인해주세요.', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+
+  const notifySuccess = () =>
+    toast.success('전송을 완료했습니다.', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+
+  const handleSubmitEmail = (e: MouseEvent<HTMLButtonElement>) => {
+    if (!title.length || !email.length || !message.length) {
+      notifyError();
+      return;
+    }
+
+    emailjs
+      .send(
+        'service_4mquxy2',
+        'template_c98c29r',
+        {
+          title,
+          from_email: email,
+          message,
+        },
+        '16eWOhhsJIXcTkcm4'
+      )
+      .then((res) => {
+        notifySuccess();
+        setTitle('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className={styles.emailFormContainer}>
-      <form className={styles.emailForm}>
-        <label htmlFor='title'>Title</label>
-        <input id='title' className={styles.titleInput} />
-        <DriveFileRenameOutlineOutlinedIcon className={styles.titleIcon} />
+    <>
+      <div className={styles.emailFormContainer}>
+        <form className={styles.emailForm}>
+          <label htmlFor='title'>Title</label>
+          <input id='title' className={styles.titleInput} value={title} onChange={handleChangeTitle} />
+          <DriveFileRenameOutlineOutlinedIcon className={styles.titleIcon} />
 
-        <label htmlFor='email' className={styles.label}>
-          Your Email
-        </label>
-        <input id='email' type='email' className={styles.emailInput} />
-        <EmailOutlinedIcon className={styles.yourEmailIcon} />
+          <label htmlFor='email' className={styles.label}>
+            Your Email
+          </label>
+          <input id='email' type='email' className={styles.emailInput} value={email} onChange={handleChangeEmail} />
+          <EmailOutlinedIcon className={styles.yourEmailIcon} />
 
-        <label htmlFor='message' className={styles.label}>
-          Message
-        </label>
-        <textarea id='message' />
+          <label htmlFor='message' className={styles.label}>
+            Message
+          </label>
+          <textarea id='message' onChange={handleChangeMessage} value={message} />
 
-        <Button className={styles.sendButton}>
-          <SendRoundedIcon />
-          Send Message
-        </Button>
-      </form>
-    </div>
+          <Button className={styles.sendButton} onClick={handleSubmitEmail}>
+            <SendRoundedIcon />
+            Send Message
+          </Button>
+        </form>
+      </div>
+
+      <ToastContainer
+        position='bottom-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+      />
+    </>
   );
 };
 
--- cobb-dev/pages/send-email/index.tsx
+++ cobb-dev/pages/send-email/index.tsx
@@ -1,31 +1,8 @@
-import emailjs from '@emailjs/browser';
-import { useState } from 'react';
 import EmailForm from './Components/EmailForm';
 import styles from './sendEmail.module.scss';
 import Greeting from './Components/Greeting';
 
 function Send_email() {
-  const [email, setEmail] = useState('');
-  const [title, setTitle] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmitEmail = (e: any) => {
-    e.preventDefault();
-    emailjs
-      .send(
-        'service_4mquxy2',
-        'template_c98c29r',
-        {
-          from_email: 'minepanorama@naver.com',
-          message: 'this is kevin',
-          title: 'hello',
-        },
-        '16eWOhhsJIXcTkcm4'
-      )
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  };
-
   return (
     <div className={styles.emailContainer}>
       <div className={styles.emailContentContainer}>
