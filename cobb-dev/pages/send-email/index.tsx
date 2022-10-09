88f11d4edbc35b1038f26f6606bd034a4949e282
Chiho <minepanorama@naver.com>
2022-10-10 03:32:19 +0900

    MOD: send-email 컴포넌트 UI수정

--- cobb-dev/pages/send-email/index.tsx
+++ cobb-dev/pages/send-email/index.tsx
@@ -1,18 +1,46 @@
-import EmailForm from './Components/EmailForm';
 import styles from './sendEmail.module.scss';
-import Greeting from './Components/Greeting';
 
 function Send_email() {
   return (
     <div className={styles.emailContainer}>
-      <div className={styles.emailContentContainer}>
-        <section className={styles.emailDesc}>
-          <Greeting />
-        </section>
-        <section className={styles.emailFormContainer}>
-          <EmailForm />
-        </section>
-      </div>
+      <section className={styles.intro}>
+        <h1>CONTACT</h1>
+        <p>Let's get to know each other.</p>
+      </section>
+
+      <section className={styles.areYou}>
+        <div className={styles.areYouIntro}>
+          <p>I'd love to hear from you.</p>
+        </div>
+
+        <div className={styles.areYouHaving}>
+          <div>
+            <p>Have</p>
+            <ul>
+              <li>• Technical Drawback ?</li>
+              <li>• Inquiry ?</li>
+            </ul>
+          </div>
+
+          <div>
+            <p>Need</p>
+            <ul>
+              <li>• Blog Improvement ?</li>
+            </ul>
+          </div>
+
+          <div>
+            <p>Want</p>
+            <ul>
+              <li>• Collaborate With ?</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.emailForm}>
+        <p>INFORM ME</p>
+      </section>
     </div>
   );
 }
