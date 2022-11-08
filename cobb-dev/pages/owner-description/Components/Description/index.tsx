1f43517ea789ae320442353bfc1bf5636b0bd285
Chiho <minepanorama@naver.com>
2022-11-09 01:49:14 +0900

    FIX: blog desc component 경로 변경

--- cobb-dev/pages/owner-description/Components/Description/index.tsx
+++ cobb-dev/pages/owner-description/Components/Description/index.tsx
@@ -1,5 +1,5 @@
-import chiho from 'public/imgs/chiho.jpg';
 import { motion } from 'framer-motion';
+import chiho from '../../../../public/imgs/chiho.jpg';
 import styles from './description.module.scss';
 
 const Description = () => {
