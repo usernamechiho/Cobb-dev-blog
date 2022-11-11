8da39e27a545d7b94e82d8163bd7f14b00a04df3
Chiho <minepanorama@naver.com>
2022-11-11 18:14:28 +0900

    DEPLOY: CORS 에러 헤더 설정

--- cobb-dev/next.config.js
+++ cobb-dev/next.config.js
@@ -5,14 +5,20 @@ const nextConfig = {
 };
 
 module.exports = {
-  images: {
-    domains: ['localhost', 'localhost:1337'],
-  },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/:path*',
-        destination: 'http://localhost:1337/:path*',
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ];
   },
